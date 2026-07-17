b22b572c4ba847b2fdbc36ba10e65b5a7aade644
Aliz <Alireza.miry.dev@gmail.com>
2026-07-17 03:37:14 +0330

    login user

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("just a test")
+while True:
+    user_input = input("Enter a command (or 'exit' to quit): ")
+    if user_input.lower() == 'exit':
+        print("Exiting the program.")
+        break
+    else:
+        print(f"You entered: {user_input}")
